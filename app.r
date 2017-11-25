f48a8fa9fd28425165c4d59f77dec7fdbb149eda
cbarlow6 <cbarlow6@student.gsu.edu>
2017-11-25 09:32:23 -0500

    Add files via upload
    corrected the drop down menu - state of assignee organization

--- app.r
+++ app.r
@@ -13,7 +13,7 @@ project_query <- qry_funs$and(
   qry_funs$gte(patent_date = "2016-01-01"),
   qry_funs$lte(patent_date = "2016-03-31")
 )
-#original dataframe
+# original dataframe
 project_result = search_pv(
   query = project_query, 
   fields = c("patent_number", 
@@ -47,13 +47,14 @@ unnested_project_result[1:5, ]
 
 #Print summary in console
 core1_df = unnested_project_result %>%  
-  summarise(patents = n_distinct(patent_number), 
-            inventors = n_distinct(inventor_id), 
-            assignees = n_distinct(assignee_id))
+  summarise("Total number of patents" = n_distinct(patent_number),
+            "Total number of inventors" = n_distinct(inventor_id),
+            "Total number of assignees" = n_distinct(assignee_id))
 
-summary(core1_df)
+
 summary_stats_dt = as.data.table(core1_df)
-str(summary_stats_dt)
+summary_stats_dt
+
 #---------------------------------------
 # Core objective #2  
 #core2_df= project_result$data$patent %>% 
@@ -90,23 +91,71 @@ result = newcore3[1:5, ]
 result
 
 # core objective #3 - bar plot 5 top assignees
-newtable = table(core2_df$assignee_organization, exclude = NA)
+newtable = table(unnested_project_result$assignee_organization, 
+                 exclude = NA)
 table3 = sort(newtable, decreasing = TRUE)
 table4 = head(table3, n = 5)
-assignees_plot = barplot(table4, xlab = "Assignee Organizations", 
-                  ylab = "Number of Patents",
+
+# horizontal bar plot
+par(mar = c(5,9,4,2))
+assignees_plot2 = barplot(table4,
+                  xlab = "Number of Patents", horiz = TRUE,
                   main = "Top Assignee Organizations",
-                  ylim = c(0, 8000),
-                  cex.names = .45,
+                  xlim = c(0, 8000),
+                  cex.names = .40,
+                  las = 2,
                   col = "blue")
-str(assignees_plot)
+
+# vertical bar plot
+par(mar=c(9,4,2,2))
+assignees_plot = barplot(table4,
+                         ylab = "Number of Patents",
+                         main = "Top Assignee Organizations",
+                         ylim = c(0, 8000),
+                         cex.names = .40,
+                         las = 2,
+                         col = "blue"
+                         
+                         )
+                        
 
 #------------------------------------
-#core objective 4
-core4_df = core2_df
+#core objective 4 - drop down menu state of assignee organization
 #-----------------------------------
-#core objective 5
+#core objective 5 - text box query investor's last name
 Inventor = patents_dt$inventor_last_name
+#-----------------------------------
+#menu objective 2
+inventor_df = unnested_project_result %>%
+  group_by (inventor_id) %>%
+  summarise(number_patents = n())
+
+
+head(inventor_df)
+
+inventor_df2 = unnested_project_result %>%
+  select(inventor_id,
+         inventor_last_name)
+ 
+
+#inventor_df3 = inventor_df %>%
+  #inner_join(inventor_df2)
+
+inventor_3 = inventor_df2 %>%
+  left_join(inventor_df) %>%
+  arrange(desc(number_patents))
+
+unique(inventor_3)
+
+inventor_joined =
+  left_join(inventor_df2, inventor_df, by = "inventor_id") %>%
+  arrange(desc(number_patents))
+  
+head(unique(inventor_joined))
+#-----------------------------------
+head(newcore3)
+head(filter(newcore3, num_patents > 10 ))
+
 
 #-----------------------------------
 # shiny app
@@ -126,22 +175,56 @@ ui <- fluidPage(
    
     # Create a spot for the barplot
     #mainPanel(
-      tabsetPanel(
-      id = 'dataset',
-      tabPanel("Summary", verbatimTextOutput("summary")),
-      tabPanel("Data Table",
-              column(4, 
-                     selectInput("assignee", "Assignee:",
-                                c("All", sort(unique(patents_dt$assignee_organization)))
-                     )
-              ),
-              hr(),
-              textInput("inventor", "Inventor's last name contains (e.g., Zeng) Note: case sensitive"),
-              hr(),
-              dataTableOutput("mytable2")
-              ),
-      tabPanel("Bar Plot", plotOutput("patentsPlot"))
-   )
+    tabsetPanel(
+    id = 'dataset',
+    tabPanel("Summary", verbatimTextOutput("summary")),
+    tabPanel("DataTable",
+      column(4, 
+        selectInput("assignee_state", "Assignee State:",
+        c("All", sort(unique(patents_dt$assignee_lastknown_state)))
+        )
+      ),
+      hr(),
+      
+      textInput("inventor", "Inventor's last name contains 
+                (e.g., Zeng) Note: case sensitive"),
+      hr(),
+      dataTableOutput("mytable2")
+    ),
+    tabPanel("AnalyzeData", 
+      # Generate a row with a sidebar
+      sidebarLayout(      
+                 
+      # Define the sidebar 
+      sidebarPanel(
+        # Input: Slider for the number of top assignees by # of patents
+        sliderInput("number",
+                    "Number of top assignees requested:",
+                    value = 5,
+                    min = 1,
+                    max = 10),
+                   hr(),
+                   #helpText("Top Assignee Organizations")
+        
+        # Input: Slider for the number of top inventors by # of patents
+        sliderInput("n",
+                    "Number of top inventors requested:",
+                    value = 5,
+                    min = 1,
+                    max = 10),
+        hr()
+        #helpText("Top Assignee Organizations")
+      ),
+
+      mainPanel(
+        plotOutput("patentsPlot"),
+        tableOutput("view")
+        )
+        
+      )
+    )
+      
+    )
   #)
  #) 
 )
@@ -162,8 +245,8 @@ server <- function(input, output) {
     mydata <- patents_dt
     
     #drop down menu - filter data table by assignee 
-    if (input$assignee != "All") {
-     mydata <- mydata[mydata$assignee_organization == input$assignee, ]
+    if (input$assignee_state != "All") {
+     mydata <- mydata[mydata$assignee_lastknown_state == input$assignee_state, ]
     }
     # text box filter by inventor
     if (!is.null(input$inventor) && input$inventor != "") {
@@ -177,16 +260,23 @@ server <- function(input, output) {
   
   #output bar plot  
   output$patentsPlot <- renderPlot({
-      # Render a barplot
-      barplot(table4, xlab = "Assignee Organizations", 
+    plot_table <- head(table3, input$number)
+    # Render a barplot
+      barplot(plot_table, 
               ylab = "Number of Patents",
               main = "Top Assignee Organizations",
               ylim = c(0, 8000),
-              cex.names = .45,
-              col = "blue"
-              )
+              cex.names = .35,
+              col = "blue")
+      
       })
 
+  #render top inventor table
+  output$view <- renderTable({
+    head(unique(inventor_joined), n = input$n)
+  })
+  
+  
 }
 shinyApp(ui = ui, server = server)
 
