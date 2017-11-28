40e4eaac9cdc4f365642542b961c1a68664d1376
cbarlow6 <cbarlow6@student.gsu.edu>
2017-11-27 20:29:45 -0500

    Add files via upload
    bug fix- related to filtering of data table

--- app.r
+++ app.r
@@ -1,7 +1,3 @@
-install.packages("patentsview")
-install.packages("tidyverse")
-install.packages("shiny")
-install.packages("data.table")
 library(patentsview)
 library(tidyverse)
 library(shiny)
@@ -24,7 +20,8 @@ project_result = search_pv(
              "inventor_lastknown_state",
              "assignee_id",
              "assignee_organization",
-             "assignee_lastknown_state"),
+             "assignee_lastknown_state",
+             "assignee_country"),
   all_pages = TRUE
 )
 # unnested original data frame
@@ -34,22 +31,12 @@ unnested_project_result = project_result$data$patents %>%
 unnested_project_result[1:5, ]
 #--------------------------------------------
 #core objective #1
-#result1 = search_pv(
-  #query = project_query, 
-  #fields = c("patent_id", "inventor_id", "assignee_id")
-  #, all_pages = TRUE
-#)
-#result1
-
-#core1 = result1$data$patents %>% 
- # unnest(inventors, .drop=FALSE) %>% 
-  #unnest(assignees)
 
 #Print summary in console
 core1_df = unnested_project_result %>%  
-  summarise("Total number of patents" = n_distinct(patent_number),
-            "Total number of inventors" = n_distinct(inventor_id),
-            "Total number of assignees" = n_distinct(assignee_id))
+  summarise("Total number of patents:" = n_distinct(patent_number),
+            "Total number of inventors:" = n_distinct(inventor_id),
+            "Total number of assignees:" = n_distinct(assignee_id))
 
 
 summary_stats_dt = as.data.table(core1_df)
@@ -57,9 +44,6 @@ summary_stats_dt
 
 #---------------------------------------
 # Core objective #2  
-#core2_df= project_result$data$patent %>% 
-  #unnest(inventors, .drop=FALSE) %>%
-  #unnest(assignees)
 
 core2_df = unnested_project_result%>%
   select(patent_number, 
@@ -94,7 +78,7 @@ result
 newtable = table(unnested_project_result$assignee_organization, 
                  exclude = NA)
 table3 = sort(newtable, decreasing = TRUE)
-table4 = head(table3, n = 5)
+head(table3, n = 5)
 
 # horizontal bar plot
 par(mar = c(5,9,4,2))
@@ -123,7 +107,6 @@ assignees_plot = barplot(table4,
 #core objective 4 - drop down menu state of assignee organization
 #-----------------------------------
 #core objective 5 - text box query investor's last name
-Inventor = patents_dt$inventor_last_name
 #-----------------------------------
 #menu objective 2
 inventor_df = unnested_project_result %>%
@@ -138,9 +121,6 @@ inventor_df2 = unnested_project_result %>%
          inventor_last_name)
  
 
-#inventor_df3 = inventor_df %>%
-  #inner_join(inventor_df2)
-
 inventor_3 = inventor_df2 %>%
   left_join(inventor_df) %>%
   arrange(desc(number_patents))
@@ -150,11 +130,34 @@ unique(inventor_3)
 inventor_joined =
   left_join(inventor_df2, inventor_df, by = "inventor_id") %>%
   arrange(desc(number_patents))
-  
+str(inventor_joined)  
 head(unique(inventor_joined))
 #-----------------------------------
+# menu objective #3
+menu3_df = unnested_project_result %>%
+  group_by(assignee_country) %>%
+  summarise(count = n())
+
+menu3_df
+colnames(menu3_df) <- c("Country", "Total")
+
+menu3 = na.omit(menu3_df) %>%
+  select(Country, Total) %>%
+  #na.exclude(assignee_org) %>%
+  arrange(desc(Total))
+
+menu3
+#menu3_result = menu3[1:5, ] 
+#menu3_result
+menu3_dt = as.data.table(menu3)
+menu3_dt
+
+#-----------------------------------
+# menu objective #4
 head(newcore3)
-head(filter(newcore3, num_patents > 10 ))
+patents_over_10 = filter(newcore3, num_patents > 10 )
+patents_over_10_dt = as.data.table(patents_over_10)
+head(patents_over_10_dt)
 
 
 #-----------------------------------
@@ -162,42 +165,40 @@ head(filter(newcore3, num_patents > 10 ))
 ui <- fluidPage(
   # Give the page a title
   titlePanel("CIS 4730 Group Project"),
-  #hr(),
   
-  # Generate a row with a sidebar
-  #sidebarLayout(
-  #,
-  
-    # Define the sidebar with one input
-    #sidebarPanel(
-     # textInput("inventor", "Inventor's last name contains (e.g., Zeng)")
-      #),
-   
-    # Create a spot for the barplot
-    #mainPanel(
+#--------------------------------    
+#tab #1 - Summary
     tabsetPanel(
     id = 'dataset',
     tabPanel("Summary", verbatimTextOutput("summary")),
+    
+#---------------------------------------------------    
+#tab #2 - DataTable
     tabPanel("DataTable",
-      column(4, 
+             
         selectInput("assignee_state", "Assignee State:",
         c("All", sort(unique(patents_dt$assignee_lastknown_state)))
-        )
-      ),
+        ),
+
       hr(),
       
-      textInput("inventor", "Inventor's last name contains 
+        textInput("inventor", "Inventor's last name contains 
                 (e.g., Zeng) Note: case sensitive"),
-      hr(),
+      
+        hr(),
+      
       dataTableOutput("mytable2")
+      
     ),
+#--------------------------------------    
+# tab #3 - AnalyzeData
     tabPanel("AnalyzeData", 
       # Generate a row with a sidebar
       sidebarLayout(      
                  
       # Define the sidebar 
       sidebarPanel(
-        # Input: Slider for the number of top assignees by # of patents
+        # Input: Slider for barplot - number of top assignees by # of patents
         sliderInput("number",
                     "Number of top assignees requested:",
                     value = 5,
@@ -212,51 +213,73 @@ ui <- fluidPage(
                     value = 5,
                     min = 1,
                     max = 10),
-        hr()
-        #helpText("Top Assignee Organizations")
-      ),
+                    hr(),
+        
+        # Input: Slider for countries are most interested in obtaining patents by assignee country
+        sliderInput("total",
+                    "Number of Countries interested in obtaining patents:",
+                    value = 5,
+                    min = 1,
+                    max = 10),
+                    hr(),
+        
+        # checkbox - show assignee org with more than 10 patents
+        checkboxInput(inputId = "over_10_patents",
+                      label = strong("Show assignee organizations
+                      with more than 10 patents"),
+                      value = FALSE),
+                      hr()
+                      ),
 
       mainPanel(
         plotOutput("patentsPlot"),
-        tableOutput("view")
+        tableOutput("view"),
+        tableOutput("country"),
+        dataTableOutput("show")
         )
-        
       )
     )
-      
-    )
-  #)
- #) 
+  )
 )
 
 
 server <- function(input, output) {
+#-----------------------------------
+# summary tab  
   
+   output$summary <- renderPrint(summary_stats_dt)  
   
-  # choose columns to display
-  #summary_stats2 = summary_stats[sample(nrow(summary_stats), 1000), ]
-  # Generate a summary of the dataset ----
-  output$summary <- renderPrint(summary_stats_dt)  
-  #output$summary <- renderDataTable(summary_stats_dt, 
-                                   # options = list(orderClasses = TRUE))
+#-----------------------------------
+# data table tab  
   
   # output data table & filter data based on selections  
   output$mytable2 <- renderDataTable({
     mydata <- patents_dt
+    state <- patents_dt$assignee_lastknown_state
     
     #drop down menu - filter data table by assignee 
     if (input$assignee_state != "All") {
-     mydata <- mydata[mydata$assignee_lastknown_state == input$assignee_state, ]
-    }
+    mydata <- mydata[state == input$assignee_state, ]
+      }
+    
     # text box filter by inventor
-    if (!is.null(input$inventor) && input$inventor != "") {
+    if (input$inventor != "") {
       inventor <- input$inventor
-      mydata <- mydata[Inventor %like% inventor, ] 
-      #%>% filter(Inventor %like% inventor)
+      mydata <- mydata[mydata$inventor_last_name %like% inventor]
     }
-    #options = list(orderClasses = TRUE)
+    
     mydata
     })  
+  
+#---------------------------------  
+# analyze data tab
+  
+  #output table - assignee organizations with more than 10 patents
+  output$show <- renderDataTable({
+    if (input$over_10_patents) {
+      patents_over_10_dt
+    }
+  })  
   
   #output bar plot  
   output$patentsPlot <- renderPlot({
@@ -271,12 +294,16 @@ server <- function(input, output) {
       
       })
 
-  #render top inventor table
+  #output top inventors table
   output$view <- renderTable({
     head(unique(inventor_joined), n = input$n)
   })
   
-  
+  #render top county table
+  output$country <- renderTable({
+    head(menu3_dt, input$total)
+  }, bordered = TRUE)  
+
 }
 shinyApp(ui = ui, server = server)
 
